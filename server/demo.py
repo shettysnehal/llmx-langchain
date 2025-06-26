e6f269421ab5c6caf3c1dfbe1de7a3e350030929
shettysnehal <shettysnehal105@gmail.com>
2025-06-26 21:28:13 +0530

    final

--- server/demo.py
+++
@@ -1,6 +0,0 @@
-from langchain_community.document_loaders import UnstructuredPDFLoader
-loader = UnstructuredPDFLoader(file_path="nlp.pdf",mode="elements", strategy="fast" )
-docs_local = []
-for doc in loader.lazy_load():
-    docs_local.append(doc)
-print("Loaded documents:", len(docs_local))
